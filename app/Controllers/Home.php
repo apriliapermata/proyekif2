8025aaf719ef8b5ac17d0d656241a629aa9a6c10
Louisaa18 <126429939+Louisaa18@users.noreply.github.com>
2023-11-24 14:59:53 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,1 +1,16 @@
 
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Coffee Dua Arah'
+        ];
+
+        return view('pages/home', $data);
+    }
+}
